51a2adab391fcf338ae07d2b2c5569fd403b22ba
Muhammad Zahab Ahmad <zabiirana27@gmail.com>
2025-09-26 12:16:05 +0500

    Internal and External storage

--- app/src/main/java/com/zahab/androidinternals/MainActivity.kt
+++ app/src/main/java/com/zahab/androidinternals/MainActivity.kt
@@ -1,84 +1,138 @@
 package com.zahab.androidinternals
 
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.ContextCompat
-import com.zahab.androidinternals.ipc.ScreenOffReceiver
-import com.zahab.androidinternals.services.MusicServiceController
+import androidx.lifecycle.lifecycleScope
 import com.zahab.androidinternals.ui.theme.AndroidInternalszahabTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : ComponentActivity() {
 
 
-    private val musicController by lazy {
-        (application as MyApplication).musicServiceController
-    }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val screenOffReceiver = ScreenOffReceiver()
         enableEdgeToEdge()
 
-        ContextCompat.registerReceiver(
-            applicationContext,
-            screenOffReceiver,
-            IntentFilter("android.intent.action.SCREEN_OFF"),
-            ContextCompat.RECEIVER_EXPORTED
-
-        )
+        lifecycleScope.launch {
+            writeToExternalStorate()
+        }
 
         setContent {
             AndroidInternalszahabTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MusicControllerUI(
-                        controller = musicController,
-                        modifier = Modifier.fillMaxSize().padding(innerPadding)
-                    )
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        var bitmap by remember {
+                            mutableStateOf<Bitmap?>(null)
+                        }
+
+                        val launcher = rememberLauncherForActivityResult(
+                            contract = ActivityResultContracts.OpenDocument()
+                        ) { uri ->
+
+                            {
+                                uri?.let {
+                                    lifecycleScope.launch {
+                                        bitmap = readUriAsBitmap(it)
+                                    }
+                                }
+                            }
+                        }
 
 
+                        Button(onClick = {
+                            launcher.launch(arrayOf("image/*"))
+                        }) {
+                            Text("Pick Photo")
+                        }
 
+                        bitmap?.let {
+                            Image(
+                                bitmap = bitmap!!.asImageBitmap(),
+                                contentDescription = null,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
 
-
-// For BroadCast
-//                    Button(
-//                        onClick = {
-//                            Intent("com.zahab.ACTION_NOTIFY_VIA_BROADCAST").also {
-//                                it.`package` = "com.zahab.andriodinternals_zahab2"
-//                                sendBroadcast(it)
-//                            }
-//                        },
-//                        modifier = Modifier
-//                            .fillMaxSize()
-//                            .wrapContentSize()
-//                            .padding(innerPadding)
-//                    ) {
-//                        Text("Send Broadcast")
-//                    }
 
                 }
             }
         }
     }
 
+    private suspend fun readUriAsBitmap(uri: Uri): Bitmap? = withContext(Dispatchers.IO) {
+
+
+        val bytes = contentResolver.openInputStream(uri)?.use { input ->
+            input.readBytes()
+        } ?: return@withContext null
+        BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+
+
+    }
+
+
     override fun onDestroy() {
         super.onDestroy()
         println("Receiver Unregistered")
     }
+
+    private suspend fun writeToInternalStorage() = withContext(Dispatchers.IO) {
+
+        val file = File(filesDir, "hello.txt")
+        FileOutputStream(file).use { outputStream ->
+            outputStream.write("Bismillah".toByteArray())
+        }
+
+    }
+
+    private suspend fun writeToExternalStorate() = withContext(Dispatchers.IO) {
+        val dir = getExternalFilesDir(null)
+        val file = File(dir, "hello.txt")
+
+        FileOutputStream(file).use { outputStream ->
+            outputStream.write("Allah o Akbar".toByteArray())
+        }
+    }
+
 }
 
 @Composable
@@ -96,3 +150,4 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
--- app/src/main/java/com/zahab/androidinternals/MyApplication.kt
+++ app/src/main/java/com/zahab/androidinternals/MyApplication.kt
@@ -1,7 +1,6 @@
 package com.zahab.androidinternals
 
 import android.app.Application
-import com.zahab.androidinternals.services.MusicServiceController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -9,15 +8,4 @@ import kotlinx.coroutines.SupervisorJob
 class MyApplication : Application() {
 
     private val applicatiokScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
-
-    lateinit var musicServiceController: MusicServiceController
-    override fun onCreate() {
-        super.onCreate()
-
-        musicServiceController = MusicServiceController(
-            context = this,
-            coroutineScope = applicatiokScope
-        )
-
-    }
 }
